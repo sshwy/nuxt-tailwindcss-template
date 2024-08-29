48396097166e5fefc522618fc26d186bd3871b34
sshwy <jy.cat@qq.com>
2024-08-29 17:28:07 +0800

    chore: bump nuxt to 3.13

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     // '@nuxt/content',
     '@nuxt/ui'
   ],
+
   css: ['~/assets/main.css'],
+  compatibilityDate: '2024-08-29',
 })
